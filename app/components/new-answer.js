c64cb641c7067d5456e93b6450f2abff6a1db62a
vincentmuya <vincentmuya13@gmail.com>
2017-10-31 17:33:38 +0300

    made changes to new-answer js

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  action: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
