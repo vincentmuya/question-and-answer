8aeb94268bfdd58d435e4e16aadefb6ba918aafd
vincentmuya <vincentmuya13@gmail.com>
2017-10-31 16:51:22 +0300

    added delete function

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(answer) {
+      if (confirm'Are you sure you want to deletethis answer?')){
+        this.sendAction('destroyAnswer', answer);
+      }
+    }
+  }
 });
