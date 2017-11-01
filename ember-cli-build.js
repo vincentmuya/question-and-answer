3c0572d83b97a1b3dd6bcde3831135ddcd591be0
vincentmuya <vincentmuya13@gmail.com>
2017-11-01 20:09:42 +0300

    modified code

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,12 @@ module.exports = function(defaults) {
     // Add options here
     sassOptions: {
       extension: 'scss'
+    },
+
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': false
     }
   });
 
